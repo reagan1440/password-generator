6eeccdfacecc62e1b856d783cf78f6eedba0e26d
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-06 12:26:00 -0500

    consts and generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const number = "0123456789";
+const specialChar = "!@#$%^&*()_-+={[}]:;'<,>.?/*";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function generatePassword() {
+  var newPassword = function (n, str) {
+    var password = "";
+    for (var i=0; i<n;i++) {
+      password += str[Math.floor(Math.random() * str.length)];
+    }
+    return password;
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
