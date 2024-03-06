1a83afdf32e6f555fc15384489d25d3d572e29fc
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-06 18:31:52 -0500

    charcter input

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,14 @@ const lower = "abcdefghijklmnopqrstuvwxyz";
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function generatePassword() {
-  var newGenPassword = function (n, str) {
-    var genPassword = "";
-    for (var i=0; i<n;i++) {
-      genPasswordpassword += str[Math.floor(Math.random() * str.length)];
-    }
-    return genPassword;
+  var passLength = PasswordLength();
+  var passwordCharacters = BuildPasswordCharacterArray();
+
+  var genPassword = "";
+  for (var i = 0; i < passLength; i++) {
+    genPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
   }
+  return genPassword;
 }
 
 var generateBtn = document.querySelector("#generate");
@@ -30,7 +31,7 @@ function PasswordLength(){
   var passLength=0;
   var validate=false;
 
-  while (!valid) {
+  while (!validate) {
     passLength = parseInt(window.prompt("choose your password length from 8-128 charcaters."));
 
     if(passLength >=8 && passLength <= 128){
@@ -43,6 +44,36 @@ function PasswordLength(){
     }
   }
 }
+
+function BuildPasswordCharacterArray() {
+  var passwordCharacters = "";
+  var confirmLowerCase = confirm("click OK for lowercase characters to be included");
+  var confirmUpperCase = confirm("click OK for uppercase characters to be included");
+  var confirmSpecialCharacter = confirm("click OK for special characters to be included");
+  var confirmNumericCharacter = confirm("click OK for numeric characters to be included");
+
+
+  if (confirmLowerCase) {
+    passwordCharacters += lower;
+  }
+  if (confirmUpperCase) {
+    passwordCharacters += upper;
+  }
+  if (confirmSpecialCharacter) {
+    passwordCharacters += specialChar;
+  }
+  if (confirmNumericCharacter) {
+    passwordCharacters += number;
+  }
+
+  if (passwordCharacters.length == 0) {
+    alert("You must select at least one character type!");
+    passwordCharacters = BuildPasswordCharacterArray(); // Call recursively to ensure at least one character type is selected
+  }
+
+  return passwordCharacters;
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
